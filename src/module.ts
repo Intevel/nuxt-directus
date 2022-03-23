42e6524ff1fb87c47d8a94c6609bc1ccd767ac1b
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-03-23 09:03:49 +0100

    chore: added options support

--- src/module.ts
+++ src/module.ts
@@ -1,24 +1,55 @@
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-import { defineNuxtModule, addPlugin } from '@nuxt/kit'
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import defu from "defu";
+import { defineNuxtModule, addPlugin } from "@nuxt/kit";
 
 export interface ModuleOptions {
-  addPlugin: boolean
+  /**
+   * Directus API URL
+   * @default process.env.DIRECTUS_URL
+   * @type string
+   */
+  url?: string;
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: "nuxt-directus",
+    configKey: "directus",
+    compatibility: {
+      nuxt: "^3.0.0",
+    },
   },
   defaults: {
-    addPlugin: true
+    url: process.env.DIRECTUS_URL,
   },
-  setup (options, nuxt) {
-    if (options.addPlugin) {
-      const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-      nuxt.options.build.transpile.push(runtimeDir)
-      addPlugin(resolve(runtimeDir, 'plugin'))
+  setup(options, nuxt) {
+    if (!options.url) {
+      throw new Error("Missing `DIRECTUS_URL` in `.env`");
     }
+
+    nuxt.options.publicRuntimeConfig.directus = defu(
+      nuxt.options.publicRuntimeConfig.directus,
+      {
+        url: options.url,
+      }
+    );
+
+    const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
+    nuxt.options.build.transpile.push(runtimeDir);
+
+    addPlugin(resolve(runtimeDir, "plugin"));
+
+    nuxt.hook("autoImports:dirs", (dirs) => {
+      dirs.push(resolve(runtimeDir, "composables"));
+    });
+  },
+});
+
+declare module "@nuxt/schema" {
+  interface ConfigSchema {
+    publicRuntimeConfig?: {
+      directus?: ModuleOptions;
+    };
   }
-})
+}
