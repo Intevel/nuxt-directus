74c0d02508fe1b36048448d304750279f3a8e6e6
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-05-14 12:48:46 +0200

    fix(chore): merge options order

--- src/module.ts
+++ src/module.ts
@@ -17,24 +17,23 @@ export default defineNuxtModule<ModuleOptions>({
     name: "nuxt-directus",
     configKey: "directus",
     compatibility: {
-      nuxt: '^3.0.0 || ^2.16.0',
-      bridge: true
-    }
+      nuxt: "^3.0.0 || ^2.16.0",
+      bridge: true,
+    },
   },
   defaults: {
     url: process.env.DIRECTUS_URL,
   },
   setup(options, nuxt) {
-    if (!options.url) {
-      throw new Error("Missing `DIRECTUS_URL` in `.env`");
-    }
-
     nuxt.options.publicRuntimeConfig.directus = defu(
       nuxt.options.publicRuntimeConfig.directus,
       {
         url: options.url,
       }
     );
+    if (!nuxt.options.publicRuntimeConfig.directus.url) {
+      throw new Error("Missing `DIRECTUS_URL` in `.env`");
+    }
 
     const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
     nuxt.options.build.transpile.push(runtimeDir);
