7602cf122938189de659cd8733adc0caac8869f5
Anoesj <anoesjsadraee@gmail.com>
2022-10-10 02:01:12 +0200

    chore: remove redundant return types and simplify `register` alias

--- src/runtime/composables/useDirectusAuth.ts
+++ src/runtime/composables/useDirectusAuth.ts
@@ -1,6 +1,5 @@
 import { useRuntimeConfig } from '#app'
 
-import type { Ref } from 'vue'
 import type {
   DirectusAuthResponse,
   DirectusAuthCredentials,
@@ -30,7 +29,7 @@ export const useDirectusAuth = () => {
     user.value = value
   }
 
-  const fetchUser = async (): Promise<Ref<DirectusUser>> => {
+  const fetchUser = async () => {
     if (token.value && !user.value) {
       try {
         if (config.directus.fetchUserParams?.filter) {
@@ -59,18 +58,15 @@ export const useDirectusAuth = () => {
     return user
   }
 
-  const login = async (
-    data: DirectusAuthCredentials
-  ): Promise<DirectusAuthResponse> => {
+  const login = async (data: DirectusAuthCredentials) => {
     setToken(null)
 
-    const response: { data: DirectusAuthResponse } = await directus(
-      '/auth/login',
-      {
-        method: 'POST',
-        body: data
-      }
-    )
+    const response: {
+      data: DirectusAuthResponse;
+    } = await directus('/auth/login', {
+      method: 'POST',
+      body: data
+    })
 
     if (!response.data.access_token) { throw new Error('Login failed, please check your credentials.') }
     setToken(response.data.access_token)
@@ -78,47 +74,34 @@ export const useDirectusAuth = () => {
     const user = await fetchUser()
 
     return {
-      user,
+      user: user.value,
       access_token: response.data.access_token,
       expires: response.data.expires
     }
   }
 
-  const createUser = async (
-    data: DirectusRegisterCredentials
-  ): Promise<DirectusUser> => {
-    return await directus('/users', {
+  const createUser = async (data: DirectusRegisterCredentials) => {
+    return await directus<DirectusUser>('/users', {
       method: 'POST',
       body: data
     })
   }
 
-  // Alias for createUser
-  const register = async (
-    data: DirectusRegisterCredentials
-  ): Promise<DirectusUser> => {
-    return createUser(data)
-  }
-
-  const requestPasswordReset = async (
-    data: DirectusPasswordForgotCredentials
-  ): Promise<void> => {
+  const requestPasswordReset = async (data: DirectusPasswordForgotCredentials) => {
     await directus('/auth/password/request', {
       method: 'POST',
       body: data
     })
   }
 
-  const resetPassword = async (
-    data: DirectusPasswordResetCredentials
-  ): Promise<void> => {
+  const resetPassword = async (data: DirectusPasswordResetCredentials) => {
     await directus('/auth/password/reset', {
       method: 'POST',
       body: data
     })
   }
 
-  const logout = (): void => {
+  const logout = () => {
     // https://docs.directus.io/reference/authentication/#logout todo: implement this
     setToken(null)
     setUser(null)
@@ -133,6 +116,7 @@ export const useDirectusAuth = () => {
     resetPassword,
     logout,
     createUser,
-    register
+    // Alias for createUser
+    register: createUser
   }
 }
