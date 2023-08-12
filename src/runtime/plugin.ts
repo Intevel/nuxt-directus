9ddea5e5a73e1a4c3fc071dfa337864f742f65b7
Sandros94 <sandro.circi@digitoolmedia.com>
2023-08-13 00:54:38 +0200

    chore: update directus sdk

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,7 +1,5 @@
-import { createDirectus } from '@directus/sdk'
+import { createDirectus, rest, authentication } from '@directus/sdk'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
-import { rest } from '@directus/sdk/rest'
-import { authentication } from '@directus/sdk/auth'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { directus: options } = useRuntimeConfig().public
