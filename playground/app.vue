91f4a379fcfe52fa627591d6c1441582c7a8a6df
Sandros94 <sandro.circi@digitoolmedia.com>
2023-08-13 00:55:40 +0200

    fix: useDirectusAuth

--- playground/app.vue
+++ playground/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     Nuxt module playground
-    <button @click="loginModule">
+    <button @click="login">
       Sign in
     </button>
     <button @click="signOut">
@@ -14,16 +14,13 @@
 </template>
 
 <script setup lang="ts">
-import { authentication } from '@directus/sdk'
-
-const directus = useDirectus().with(authentication())
 const { signIn, signOut, refreshTokens } = useDirectusAuth()
 
 const login = async () => {
   const t = await signIn('admin@example.com', 'Passw0rd!')
+  // eslint-disable-next-line no-console
   console.log(t)
 }
-
 // console.log(await directus.login('admin@example.com', 'Passw0rd!'))
 // console.log(await signIn('admin@example.com', 'Passw0rd!', {}))
 </script>
--- src/runtime/composables/useDirectusAuth.ts
+++ src/runtime/composables/useDirectusAuth.ts
@@ -1,52 +1,50 @@
 /* eslint-disable camelcase */
-import { authentication, type loginOptions } from '@directus/sdk'
+import { rest, login, refresh, logout } from '@directus/sdk'
 import { useDirectus } from './useDirectus'
 import { useDirectusCookie } from './useDirectusCookie'
 
 export function useDirectusAuth () {
-  const directus = useDirectus().with(authentication())
-  const { token, refreshToken } = useDirectusCookie()
+  const directus = useDirectus().with(rest())
+  const { accessToken, refreshToken } = useDirectusCookie()
 
   const signIn = async (identifier: string, password: string) => {
     try {
-      const { access_token, expires, refresh_token, expires_at } = await directus.login(identifier, password, {})
-      token().value = access_token
-      refreshToken().value = refresh_token
-
-      return {
-        access_token,
-        expires,
-        refresh_token,
-        expires_at
+      const { access_token, expires, refresh_token, expires_at } = await directus.request(login(identifier, password, {}))
+      /* the following `if` is required to avoid a type error
+      *  because the type of expires is number | null
+      *  while maxAge for useCookie is number | undefined
+      */
+      if (expires !== null) {
+        accessToken(expires).value = access_token
+        refreshToken(expires).value = refresh_token
       }
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error)
     }
   }
 
   const refreshTokens = async () => {
     try {
-      const { access_token, expires, refresh_token, expires_at } = await directus.refresh()
-      token().value = access_token
-      refreshToken().value = refresh_token
-
-      return {
-        access_token,
-        expires,
-        refresh_token,
-        expires_at
+      const { access_token, expires, refresh_token, expires_at } = await directus.request(refresh(refreshToken().value!))
+      // check previous note about type error
+      if (expires !== null) {
+        accessToken(expires).value = access_token
+        refreshToken(expires).value = refresh_token
       }
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error)
     }
   }
 
   const signOut = async () => {
     try {
-      await directus.logout()
-      token().value = null
+      await directus.request(logout(refreshToken().value!))
+      accessToken().value = null
       refreshToken().value = null
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error)
     }
   }
