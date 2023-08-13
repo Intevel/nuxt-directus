6573d941072a715d66de5611ea5fc2c889d35215
Sandros94 <sandro.circi@digitoolmedia.com>
2023-08-13 02:38:11 +0200

    feat: useDirectusGraphql

--- src/runtime/composables/useDirectus.ts
+++ src/runtime/composables/useDirectus.ts
@@ -1,8 +1,10 @@
 import {
   type DirectusClient,
-  rest,
+  type GraphqlClient,
+  type RestClient,
   type RestConfig,
-  type RestClient
+  graphql,
+  rest
 } from '@directus/sdk'
 import { useNuxtApp } from '#imports'
 
@@ -12,4 +14,8 @@ export const useDirectus = <T extends Object>() => {
 
 export const useDirectusRest = <T extends Object>(config?: RestConfig) => {
   return useNuxtApp().$directus.with(rest(config)) as RestClient<T>
+}
+
+export const useDirectusGraphql = <T extends Object>() => {
+  return useNuxtApp().$directus.with(graphql()) as GraphqlClient<T>
 }
