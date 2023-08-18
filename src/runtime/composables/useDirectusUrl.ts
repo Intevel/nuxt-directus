ad28feeb19fd8b3b321ede816db9648f63af1c34
Rashid Pathiyil <rashidpathiyil@gmail.com>
2023-08-18 19:48:13 +0530

    feat: remove trailing slash if present

--- src/runtime/composables/useDirectusUrl.ts
+++ src/runtime/composables/useDirectusUrl.ts
@@ -2,5 +2,5 @@ import { useRuntimeConfig } from '#app'
 
 export const useDirectusUrl = (): string => {
   const config = useRuntimeConfig()
-  return config.public.directus.url
+  return config.public.directus.url.replace(/\/$/, '')
 }
