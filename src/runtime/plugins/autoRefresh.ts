09a63d509ab2867227147023c5c37b90d7c4e302
Sandros94 <sandro.circi@digitoolmedia.com>
2023-09-11 02:16:32 +0200

    feat: validate/invalidate user authentication

--- src/runtime/plugins/autoRefresh.ts
+++ src/runtime/plugins/autoRefresh.ts
@@ -2,29 +2,31 @@ import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { fetchUser, refreshTokens } = useDirectusAuth()
-  const { refreshToken, accessToken } = useDirectusCookie()
+  const { accessToken, refreshToken } = useDirectusCookie()
   const user = useDirectusUser()
 
-  async function checkIfAccessTokenExpired () {
-    if (!accessToken().value && refreshToken().value) {
-      await refreshTokens()
-    }
-  }
-
-  async function checkIfUserExists () {
-    if (!user.value && accessToken().value && refreshToken().value) {
-      await fetchUser()
+  async function checkUserAuth () {
+    if (!user.value && accessToken().value) {
+      await fetchUser().catch(async (e) => {
+        if (e.errors[0].extensions.code === 'TOKEN_EXPIRED') {
+          nuxtApp.runWithContext(async () => await refreshTokens()).catch((e) => {
+            if (e.errors[0].extensions.code === 'TOKEN_EXPIRED') {
+              accessToken().value = null
+              refreshToken().value = null
+            }
+          })
+        }
+      })
     }
   }
 
   // do the checks server-side, instead of using hook 'app:created',
   // as this hook is not called on SSR=true (static generation)
-  await checkIfAccessTokenExpired()
-  await checkIfUserExists()
+  await checkUserAuth()
 
   nuxtApp.hook('page:start', async () => {
     if (process.client) {
-      await checkIfUserExists()
+      await checkUserAuth()
     }
   })
 })
