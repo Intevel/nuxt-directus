b6f5a0c17fe15b001dffec67f2cd56e910e59422
Sandros94 <sandro.circi@digitoolmedia.com>
2023-09-21 19:21:12 +0200

    fix: useDirectusUser types

--- src/runtime/composables/useDirectusUser.ts
+++ src/runtime/composables/useDirectusUser.ts
@@ -1,5 +1,5 @@
 import type { DirectusUser } from '../types'
 import { type Ref, useState } from '#imports'
 
-export const useDirectusUser = <T extends DirectusUser<any>>(): Ref<T | null> =>
-  useState<T | null>('directus.user')
+export const useDirectusUser = <T extends object>(): Ref<DirectusUser<T> | null> =>
+  useState<DirectusUser<T> | null>('directus.user', () => null)
