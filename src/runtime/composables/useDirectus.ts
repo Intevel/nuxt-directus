daab58a86b6c086c780d9ebc4205f80bb09be550
Sandros94 <sandro.circi@digitoolmedia.com>
2023-09-25 16:58:56 +0200

    Revert "fix: graphql doesn't have type hinting in sdk atm"
    This reverts commit a8d82848649e956470bf313b40982f198907d38e.

--- src/runtime/composables/useDirectus.ts
+++ src/runtime/composables/useDirectus.ts
@@ -52,11 +52,11 @@ export const useDirectusRest = <T extends Object>(config?: DirectusRestConfig) =
   return client
 }
 
-export const useDirectusGraphql = (config?: DirectusGrafqlConfig) => {
+export const useDirectusGraphql = <T extends Object>(config?: DirectusGrafqlConfig) => {
   const { autoRefresh, cookieConfigs } = useRuntimeConfig().public.directus
   const publicStaticToken = ref('')
 
-  const client = useDirectus().with(authentication(
+  const client = useDirectus<T>().with(authentication(
     cookieConfigs.useNuxtCookies ? 'json' : 'cookie', {
       autoRefresh,
       credentials: 'include',
