d9f2480bb2eeb1226ebafd4462f61bfb7b60ef4d
Sandros94 <sandro.circi@digitoolmedia.com>
2023-09-25 16:23:34 +0200

    fix: autoRefresh check for missing accessToken too

--- src/runtime/plugins/autoRefresh.ts
+++ src/runtime/plugins/autoRefresh.ts
@@ -1,14 +1,14 @@
 import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const { user, refreshTokens } = useDirectusAuth()
+  const { user, tokens, refreshTokens } = useDirectusAuth()
   const { refreshToken } = useDirectusTokens()
   const { useNuxtCookies } = useRuntimeConfig().public.directus.cookieConfigs
   
   // TODO: check for cookies, to avoid unnecessary requests
   async function checkUserAuth () {
     if (process.client) {
-      if (!user.value && (useNuxtCookies ? refreshToken().value : true)) {
+      if ((!tokens.value?.access_token || !user.value) && (useNuxtCookies ? refreshToken().value : true)) {
         await refreshTokens().catch((error) => error)
       }
     }
