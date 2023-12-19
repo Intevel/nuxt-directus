0755ee002587d2bec56a0817adeadfddea9bb82e
Sandros94 <sandro.circi@digitoolmedia.com>
2023-12-19 14:02:53 +0100

    init: local directus dev instance

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
       autoImport: true
     },
     authConfig: {
-      useNuxtCookies: true
+      refreshTokenCookieName: 'nuxt-directus_refresh_token'
     }
   }
 })
