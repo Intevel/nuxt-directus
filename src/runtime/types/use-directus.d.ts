1869a17cafe429bce315f8e7fe6112119df4e3de
Sandros94 <sandro.circi@digitoolmedia.com>
2024-01-07 19:55:40 +0100

    refactor: directus clients improving types and functionalities

--- src/runtime/types/use-directus.d.ts
+++ src/runtime/types/use-directus.d.ts
@@ -1,4 +1,5 @@
 import {
+  AuthenticationConfig,
   ClientOptions,
   RestConfig,
   GraphqlConfig,
@@ -12,7 +13,7 @@ export interface DirectusClientOptions {
    * The URL of the Directus instance. The default value is defined in the Nuxt runtime config.
    */
   url?: string;
-  options?: ClientOptions;
+  clientOptions?: ClientOptions;
   fetchOptions?: Omit<FetchOptions, 'baseURL'>;
 }
 
@@ -23,12 +24,17 @@ export interface DirectusClientConfig {
    * @type boolean | string | undefined
   */
   useStaticToken?: boolean | string | undefined;
-  clientOptions?: DirectusClientOptions;
+  options?: DirectusClientOptions;
+  authConfig: Partial<AuthenticationConfig>;
 }
 
-export interface DirectusRestConfig extends DirectusClientConfig, RestConfig {}
+export interface DirectusRestConfig extends DirectusClientConfig {
+  restConfig?: RestConfig;
+}
 
-export interface DirectusGraphqlConfig extends DirectusClientConfig, GraphqlConfig {}
+export interface DirectusGraphqlConfig extends DirectusClientConfig {
+  graphqlConfig?: GraphqlConfig;
+}
 
 export interface DirectusRealtimeConfig extends DirectusClientConfig {
   websocketConfig?: WebSocketConfig;
