bd37a3ab1ee9f9db13fa42834bd1ef743460907a
Sandros94 <sandro.circi@digitoolmedia.com>
2024-01-11 18:19:34 +0100

    feat: server util useDirectusRealtime

--- src/runtime/server/utils/use-directus.ts
+++ src/runtime/server/utils/use-directus.ts
@@ -1,15 +1,23 @@
 import { defu } from 'defu'
 import {
   createDirectus,
+  realtime,
   rest,
   staticToken
 } from '@directus/sdk'
+import { WebSocket } from 'ws'
 import type {
   DirectusClientOptions,
-  DirectusRestConfig
+  DirectusRestConfig,
+  DirectusRealtimeConfig
 } from '../../types'
 import { useRuntimeConfig } from '#imports'
 
+/**
+ * Provides the core functionalities to create a custom Directus client.
+ *
+ * @returns createDirectus.
+ */
 export const useDirectus = <T extends Object>(options?: Partial<DirectusClientOptions>) => {
   const { url } = useRuntimeConfig().public.directus
 
@@ -17,7 +25,8 @@ export const useDirectus = <T extends Object>(options?: Partial<DirectusClientOp
     baseURL: url,
     clientOptions: {
       globals: {
-        fetch: $fetch.create(options?.fetchOptions ?? {})
+        fetch: $fetch.create(options?.fetchOptions ?? {}),
+        WebSocket
       }
     }
   }
@@ -31,6 +40,11 @@ export const useDirectus = <T extends Object>(options?: Partial<DirectusClientOp
   return createDirectus<T>(config.baseURL, config.clientOptions)
 }
 
+/**
+ * Creates a client to communicate with the Directus REST API.
+ *
+ * @returns A Directus REST client.
+ */
 export const useDirectusRest = <T extends Object>(options?: Partial<Omit<DirectusRestConfig, 'authConfig'>>) => {
   const { staticToken: privateToken } = useRuntimeConfig().directus
 
@@ -51,6 +65,33 @@ export const useDirectusRest = <T extends Object>(options?: Partial<Omit<Directu
 
   const client = useDirectus<T>(config?.options)
     .with(rest(config.restConfig))
+    .with(staticToken(token))
+
+  return client
+}
+
+/**
+ * Creates a client to communicate with the Directus Realtime API.
+ *
+ * @returns A Directus Realtime client.
+ */
+export const useDirectusRealtime = <T extends Object>(options?: Partial<DirectusRealtimeConfig>) => {
+  const { staticToken: privateToken } = useRuntimeConfig().directus
+
+  const defaultOptions: Partial<Omit<DirectusRealtimeConfig, 'authConfig'>> = {
+    useStaticToken: true
+  }
+
+  const config = defu(options, defaultOptions)
+
+  const token = config.useStaticToken === true
+    ? privateToken
+    : typeof config.useStaticToken === 'string'
+      ? config.useStaticToken
+      : ''
+
+  const client = useDirectus<T>(config?.options)
+    .with(realtime(config?.websocketConfig))
     .with(staticToken(token))
 
   return client
