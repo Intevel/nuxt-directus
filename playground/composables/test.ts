1c0f39aac1aee1e59508508e6ec766f05a6f39de
Sandros94 <sandro.circi@digitoolmedia.com>
2024-02-05 17:10:08 +0100

    chore: playground update useAsyncData debug

--- playground/composables/test.ts
+++ playground/composables/test.ts
@@ -1,6 +1,8 @@
+import { hash } from 'ohash'
 import type { Ref } from 'vue'
 import { readItems as readItemsSDK } from '@directus/sdk'
 import { useAsyncData, useDirectusItems } from '#imports'
+import type { RegularCollections, Query, CollectionType, DirectusItemsOptionsAsyncData } from '../../src/runtime/types'
 import type { Schema } from '../types'
 
 export function myComposable () {
@@ -18,7 +20,34 @@ export function myComposable () {
     )
   }
 
+  async function testReadItems <
+  Collection extends RegularCollections<Schema>,
+  TQuery extends Query<Schema, CollectionType<Schema, Collection>>
+  > (
+    collection: MaybeRef<Collection>,
+    params?: DirectusItemsOptionsAsyncData<TQuery>
+  ) {
+    const collectionRef = toRef(collection) as Ref<Collection>
+    const queryRef = params?.query ? reactive<TQuery>(params.query) : undefined
+    const key = computed(() => {
+      return hash([
+        'readItems',
+        unref(collectionRef),
+        'only-a-test'
+      ])
+    })
+    return await useAsyncData(
+      params?.key ?? key.value,
+      () => Promise.resolve([{
+        collection: collectionRef.value,
+        query: queryRef
+      }]),
+      params?.params
+    )
+  }
+
   return {
-    readItemsTest
+    readItemsTest,
+    testReadItems
   }
 }
--- playground/pages/test.vue
+++ playground/pages/test.vue
@@ -2,19 +2,23 @@
   <div>
     <input v-model="fieldParam" type="text" placeholder="Field Param">
     <input v-model="searchParam" type="text" placeholder="Search posts">
-    <button @click="refresh(); refreshToo(); refreshThree()">
+    <button @click="refresh(); refreshToo(); refreshThree(); refreshFour();">
       Refresh
     </button>
     <pre v-if="notWorking && notWorking.length > 0">
-      useAsyncData
+      useAsyncData (not working)
       {{ notWorking }}
     </pre>
+    <pre v-if="notWorkingToo && notWorkingToo.length > 0">
+      Local Composable (not working)
+      {{ notWorkingToo }}
+    </pre>
     <pre v-if="working && working.length > 0">
-      SDK
+      SDK (working)
       {{ working }}
     </pre>
     <pre v-if="workingToo && workingToo.length > 0">
-      myComposable
+      myComposable (working)
       {{ workingToo }}
     </pre>
   </div>
@@ -53,8 +57,19 @@ const { data: working, refresh: refreshToo } = await useAsyncData(
   }
 )
 
-const { readItemsTest } = myComposable()
+const { readItemsTest, testReadItems } = myComposable()
 
 const { data: workingToo, refresh: refreshThree } =
   await readItemsTest(fieldParam, searchParam)
+const { data: notWorkingToo, refresh: refreshFour } =
+  await testReadItems('posts', {
+    query: {
+      fields: [fieldParam.value],
+      search: searchParam.value
+    },
+    params: {
+      immediate: false,
+      watch: [fieldParam, searchParam]
+    }
+  })
 </script>
