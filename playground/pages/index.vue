e1d5e2e30c01a19c5c9d3565bc5a13107e32b33a
Sandros94 <sandro.circi@digitoolmedia.com>
2024-09-03 16:59:48 +0200

    chore: update playground

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,28 +1,11 @@
 <template>
   <div>
-    <div>
-      <input v-model="search" type="text">
-      <input v-model="limit" type="number">
-    </div>
-    <div>
-      <pre>
-        {{ data }}
-      </pre>
-    </div>
+    <ul>
+      <li>
+        <NuxtLink to="/items">
+          useDirectusItems
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
-
-<script setup lang="ts">
-import { useDirectusItems } from '#imports'
-
-const { readItems } = useDirectusItems()
-const search = ref('')
-const limit = ref(1)
-
-const { data } = await readItems('pages', {
-  fields: ['id', 'title'],
-  search,
-  limit,
-  watch: [limit, search],
-})
-</script>
---
+++ playground/pages/items.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+      <label>
+        Search:<br>
+        <input v-model="search" type="text">
+      </label>
+      <br>
+      <label>
+        Limit:<br>
+        <input v-model="limit" type="number">
+      </label>
+    </div>
+    <div>
+      <pre>
+        {{ data }}
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDirectusItems } from '#imports'
+
+interface Page {
+  date_created: Date
+  date_updated: Date
+  id: string
+  seo: string
+  sort: number | null
+  status: 'published' | 'draft' | 'archived'
+  title: string
+  user_created: string
+  user_updated: string
+  permalink: string
+  blocks: string[] | Record<string, unknown>[]
+}
+
+const { readItems } = useDirectusItems()
+const search = ref('')
+const limit = ref(1)
+
+const { data } = await readItems<Page[]>('pages', {
+  search,
+  limit,
+  watch: [limit, search],
+})
+</script>
