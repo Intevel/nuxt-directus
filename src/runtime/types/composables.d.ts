5214ac75cc8cb66c38072b0e2f7d8d752740af68
Sandros94 <sandro.circi@digitoolmedia.com>
2024-10-02 14:18:05 +0200

    fix: missing export for new composables

--- src/runtime/types/composables.d.ts
+++ src/runtime/types/composables.d.ts
@@ -1,7 +1,10 @@
+export * from '#directus/composables/activity'
 export * from '#directus/composables/collections'
 export * from '#directus/composables/fetch'
 export * from '#directus/composables/items'
 export * from '#directus/composables/notifications'
+export * from '#directus/composables/revisions'
+export * from '#directus/composables/translations'
 
 // TODO: move them to related composables
 // export type * from './use-directus-sdk'
